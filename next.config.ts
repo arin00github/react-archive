5918c2fed4ff7dbd02785451f3822e609bdc86d1
arin00github <teru325@naver.com>
2025-11-12 12:36:45 +0900

    🔨 config : github 배포를 위한 설정 및 스크립트 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "react-archive";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // << 핵심: 정적 export
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: { unoptimized: true }, // next/image 정적 내보내기
+  trailingSlash: true, // GitHub Pages에서 정적 라우팅 편해짐
+
   compiler: {
     styledComponents: true,
   },

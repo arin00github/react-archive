5862889762ba2ba042ec2a26efda1773fe3bc424
arin00github <teru325@naver.com>
2026-02-07 23:09:27 +0900

    🔥remove: country 페이지 삭제

--- src/app/(archive)/diplomacy/[country]/page.tsx
+++
@@ -1,7 +0,0 @@
-import DiplomacyDetailContainer from "@/components/(archive)/diplomacy/detail";
-
-const WorldDiplomacyDetailPage = () => {
-  return <DiplomacyDetailContainer />;
-};
-
-export default WorldDiplomacyDetailPage;
